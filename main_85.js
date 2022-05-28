efe008a2c1a9a6ff7f1e5b911a045dfa5d6c5769
postingthecode1 <103526152+postingthecode1@users.noreply.github.com>
2022-05-27 19:13:29 -0700

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,30 @@
+menu_list_array = [ "Veg Margherita Pizza",                  
+"Cheese Pizza", "Pepperoni Pizza", "Paneer Pizza", "Veggie Pizza"]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion()
+{
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("menu_div").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+
+    
+}
+function getmenu()
+{
+        //Give the appropriate id name as add_item
+        var item = document.getElementById("add_item").value;
+        //Use the push() function to push the item into menu_list_array
+        pizza_list_array.push(item);
+}
